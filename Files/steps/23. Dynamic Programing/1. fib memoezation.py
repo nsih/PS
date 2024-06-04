b8220933be10ee205811d7cda2225f626d7ab8ca
nsih6571 <nsih6571@gmail.com>
2024-06-04 16:27:21 +0900

    240604

---
+++ Files/steps/23. Dynamic Programing/1. fib memoezation.py
@@ -0,0 +1,10 @@
+import sys
+def fib1(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fib1(n - 1) + fib1(n - 2)
+N = int(sys.stdin.readline())
+print(fib1(N),N-2)
