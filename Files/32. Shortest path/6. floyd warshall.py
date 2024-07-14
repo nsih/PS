0a2a36e2b4d14ea12fd447cb2670d6354967262a
nsih6571 <nsih6571@gmail.com>
2024-07-14 18:15:06 +0900

    240714

--- Files/32. Shortest path/6. floyd warshall.py
+++ Files/32. Shortest path/6. floyd warshall.py
@@ -17,34 +17,22 @@ def floyd_warshall(n,graph):
         for j in range(n):
             if dist[i][j] == INF:
                 dist[i][j] = 0
+
     return dist
 
 
 import sys
 INF = int(1e9)
 
-n,m = map(int,sys.stdin.readline().split())
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
 
 edges = []
 for _ in range(m):
     a, b, c = map(int, sys.stdin.readline().split())
-    edges.append((a - 1, b - 1, c))
+    edges.append((a-1, b-1, c))
 
 result = floyd_warshall(n,edges)
 
-resultD = []
-for i in range(n):
-    for j in range(i + 1, n):
-        if result[i][j] and result[j][i]:
-            resultD.append(result[i][j] + result[j][i])
-
-
-
-'''
 for row in result:
     print(*row)
-'''
-if len(resultD):
-    print(min(resultD))
-else:
-    print(-1)
